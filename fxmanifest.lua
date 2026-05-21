2dd8c09cac087f4595f54bfc518d80d4128f2a44
Winnie DaPooh <scaudill3169@gmail.com>
2026-05-21 03:32:49 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Winnie 🐻'
+description 'Winnie EUP Pack — MP Male & Female Freemode'
+version '1.0.0'
+
+files {
+    -- FEMALE (mp_f_freemode_01)
+    'stream/female/**/**/*.ydd',
+    'stream/female/**/**/*.ytd',
+    'stream/female/**/**/*.ydr',
+    'stream/female/**/**/*.yft',
+
+    -- MALE (mp_m_freemode_01)
+    'stream/male/**/**/*.ydd',
+    'stream/male/**/**/*.ytd',
+    'stream/male/**/**/*.ydr',
+    'stream/male/**/**/*.yft',
+
+    -- SHARED PROPS
+    'stream/shared/**/**/*.ydd',
+    'stream/shared/**/**/*.ytd',
+    'stream/shared/**/**/*.ydr',
+    'stream/shared/**/**/*.yft'
+}
+
+-- OPTIONAL: Only keep if you have .ytyp files
+-- data_file 'DLC_ITYP_REQUEST' 'stream/shared/props/props.ytyp'
